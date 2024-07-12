fb5581f2898ade0a7a21a25fc907f75da7b89997
Christina Manara <manara1024@gmail.com>
2024-07-12 10:30:12 +0300

    initial commit

---
+++ Transformer from scratch/model.py
@@ -0,0 +1,39 @@
+import torch 
+import torch.nn as nn 
+import math
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int): # dimention of the model - 512, vocabulary size
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEnconding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None: 
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len, 1) 
+        position = torch.arange(0, seq_len, dtype=float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # Apply the sin to even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0) # add a dimentio for batch (1, seq_len, d_model)
+
+        self.register_buffer('pe', pe) # save the pe
+
+    def forward(self, x):
+        x = x + self.pe[:, :x.shape[1], :].requires_grad(False) # do not recalculate this tensors that is why we use the requires_grad
+        return self.dropout(x)
