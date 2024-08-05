4534e3dfcf8a4f5271ec5549968315a457bc8afa
Christina Manara <manara1024@gmail.com>
2024-08-05 17:40:13 +0300

    feature: chat ui and message component

--- RAG with Langchain and Chainlit/chat.py
+++ RAG with Langchain and Chainlit/chat.py
@@ -1,6 +1,45 @@
+import chainlit as cl
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+
 import os
 from dotenv import load_dotenv
 
 load_dotenv() # Load the environment variables
 
-OPEN_AI_KEY = os.getenv('OPENAI_API_KEY')
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+
+from langchain_openai import ChatOpenAI
+
+@cl.on_chat_start
+async def on_chat_start():
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | llm | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable") 
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
