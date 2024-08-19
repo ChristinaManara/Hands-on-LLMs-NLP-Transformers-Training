e38447cefe3e88c61bbfc86515b499e5f67e1011
Christina Manara <manara1024@gmail.com>
2024-08-19 16:51:49 +0300

    rebuild: issue on_chat_start() func

--- RAG with Langchain and Chainlit/chat.py
+++ RAG with Langchain and Chainlit/chat.py
@@ -18,10 +18,14 @@ from langchain.prompts.chat import (
 )
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableConfig
+from langchain.memory import VectorStoreRetrieverMemory, ConversationBufferMemory
+from langchain.chains import LLMChain, ConversationalRetrievalChain
+import pymupdf
 
 from pypdf import PdfReader
 from io import BytesIO
 import os
+import asyncio
 
 from dotenv import load_dotenv
 
@@ -31,38 +35,10 @@ os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv('LANGCHAIN_API_KEY')
 
-# Define the prompt template
-prompt = PromptTemplate(
-    template="You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. \
-    If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise. \
-    Question: {question} \
-    Context: {context} \
-    Answer:",
-    input_variables=["question", "context"]
-)
-
-# system_template = """Use the following pieces of context to answer the users question.
-# If you don't know the answer, just say that you don't know, don't try to make up an answer.
-# ALWAYS return a "SOURCES" part in your answer.
-# The "SOURCES" part should be a reference to the source of the document from which you got your answer.
-
-# Example of your response should be:
-
-# ```
-# The answer is foo
-# SOURCES: xyz
-# ```
-
-# Begin!
-# ----------------
-# {summaries}"""
-
-# messages = [
-#     SystemMessagePromptTemplate.from_template(system_template),
-#     HumanMessagePromptTemplate.from_template("{question}"),
-# ]
-#prompt = ChatPromptTemplate.from_messages(messages)
-#chain_type_kwargs = {"prompt": prompt}
+#model_name = "mixedbread-ai/mxbai-embed-large-v1"
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {'device': 'cpu'}
+hf = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -85,15 +61,16 @@ async def on_chat_start():
         content=f"`{file_name}` is uploaded."
     ).send()
 
-    msg = cl.Message(content=f"Processing `{file.name}`...")
-    await msg.send()
+    cl.user_session.set("file", file)
 
-    # Create a pdf reader object
-    reader = PdfReader(file.path)
+@cl.on_message # this function will be called every time a user inputs a message in the UI
+async def main(message: str):
+    file = cl.user_session.get("file")
 
+    doc = pymupdf.open(file.path)
     pdf_text = ""
-    for page in reader.pages:
-        pdf_text += page.extract_text()
+    for page in doc:
+        pdf_text += page.get_text()
 
     docs = Document(page_content=pdf_text)
 
@@ -101,83 +78,56 @@ async def on_chat_start():
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
     splits = text_splitter.split_documents([docs])
 
-    # Create metadata for each chunk
-    metadatas = [{"source": f"{i}-pl"} for i in range(len(splits))]
-    
-    # Create the embedding
-    model_name = "mixedbread-ai/mxbai-embed-large-v1"
-    hf = HuggingFaceEmbeddings(
-        model_name=model_name,
-    )
-
     # Create the FAISS vector store
     vectorstore = FAISS.from_documents(splits, hf)
-    retriever = vectorstore.as_retriever()
 
     # LLM
-    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.4)
 
     def format_docs(docs):
         return "\n\n".join([d.page_content for d in docs])
 
-    # Create chain
-    chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
+    history = []
+    # Define the prompt template
+    promptHist = PromptTemplate(
+        template="You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. \
+        If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise. \
+        Chat History: {chat_history} \
+        Question: {question} \
+        Context: {context} \
+        Answer:",
+        input_variables=["question", "context"]
     )
 
-    # chain = RetrievalQAWithSourcesChain.from_chain_type(
-    #     ChatOpenAI(model_name="gpt-4o-mini", temperature=0),
-    #     chain_type="stuff",
-    #     retriever=retriever,
-    # )
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True, output_key='answer')
 
-    # Save the metadata and texts in the user session
-    cl.user_session.set("metadatas", metadatas)
-    cl.user_session.set("texts", splits)
+    retriever = vectorstore.as_retriever(
+        search_type="similarity",  # Also test "similarity", "mmr"
+        search_kwargs={"k": 2},)
 
-    # Create user session to store data
-    cl.user_session.set("chain", chain)
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True,
+        verbose = True,
+        combine_docs_chain_kwargs={'prompt': promptHist},
+        memory = memory,
+        get_chat_history=lambda h : h,
+    )
 
+    # answer = qa({"question" : message.content, "chat_history" : history})
+    # history.append((message.content, answer))
+    # print(answer)
+    
+    # # res = await cl.make_async(llm_chain.run)(message.content)
+    # await cl.Message(content=answer).send()
+    # Get the answer from the ConversationalRetrievalChain
+    response = await cl.make_async(qa.invoke)(
+        {"question": message.content, "chat_history": history},
+        callbacks=[cl.LangchainCallbackHandler()]
+    )
+    
+    # Append the response to the history
+    history.append({"question": message.content, "answer": response['answer']})
 
-
-@cl.on_message # this function will be called every time a user inputs a message in the UI
-async def main(message: str):
-    chain = cl.user_session.get("chain")  # type: RetrievalQAWithSourcesChain
-    msg = cl.Message(content="")
-
-    class PostMessageHandler(BaseCallbackHandler):
-        """
-        Callback handler for handling the retriever and LLM processes.
-        Used to post the sources of the retrieved documents as a Chainlit element.
-        """
-
-        def __init__(self, msg: cl.Message):
-            BaseCallbackHandler.__init__(self)
-            self.msg = msg
-            self.sources = set()  # To store unique pairs
-
-        def on_retriever_end(self, documents, *, run_id, parent_run_id, **kwargs):
-            for d in documents:
-                source_page_pair = (d.metadata['source'], d.metadata['page'])
-                self.sources.add(source_page_pair)  # Add unique pairs to the set
-
-        def on_llm_end(self, response, *, run_id, parent_run_id, **kwargs):
-            if len(self.sources):
-                sources_text = "\n".join([f"{source}#page={page}" for source, page in self.sources])
-                self.msg.elements.append(
-                    cl.Text(name="Sources", content=sources_text, display="inline")
-                )
-
-    async for chunk in chain.astream(
-        message.content,
-        config=RunnableConfig(callbacks=[
-            cl.LangchainCallbackHandler(),
-            PostMessageHandler(msg)
-        ]),
-    ):
-        await msg.stream_token(chunk)
-
-    await msg.send()
+    # Send the response back to the user
+    await cl.Message(content=f"Answer: {response['answer']}").send()
